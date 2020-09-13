dd801b1326f052e1b1913c43b450216caeede2f0
Rodrigo Silva <rs4812575@gmail.com>
2020-09-12 23:42:05 -0300

    Questão-1 do Trabalho 1 de POO

--- 1quest_o1.py
+++ 1quest_o1.py
@@ -1,4 +1,5 @@
 valores = []
+# Capturando os valores/itens da lista.
 while True:
     numero = int(input("numero: "))
     if numero != -1:
@@ -6,6 +7,11 @@ while True:
     else:
         print(valores)
         break
+# Mudando o separador da lista de "," para ";".
 print (*valores, sep=(";"))
+# Revertendo a ordem da lista e mudando o separador da mesma de "," para "#".
 valores.reverse()
 print(*valores, sep=("#"))
+#Somando os itens ou valores da lista.
+soma = sum(valores)
+print(soma)
