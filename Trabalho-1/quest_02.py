021ec9ff69ef97a0faf08d5afc2ebfc448e2c690
rodrigo620 <rs4812575@gmail.com>
2020-09-13 17:12:42 -0300

    Falta atualizar a alternativa A

--- Trabalho-1/quest_02.py
+++ Trabalho-1/quest_02.py
@@ -3,13 +3,35 @@ numeros = []
 for voltar in range(10):
     valor = int(input("numero "))
     numeros.append(valor)
-print(numeros)
 copia_lista = numeros [:]
-print(copia_lista)
+# A) Exibindo os indices do numeros maiores que 50.
 maior50 = []
-for unidade in copia_lista:
-    if unidade > 50:
-        maior50.append(unidade)
-print(maior50)
+for a in copia_lista:
+    if a > 50:
+        maior50.append(a)
+        pass
+if len(maior50) != 0:
+        print(maior50)
+        pass
 if len(maior50) == 0:
     print("Alista não apresenta elementos maiores que o número 50.")
+    pass
+# B) Removendo elementos entre 10 e 20.
+intervalo = []  
+remoção = copia_lista [:]
+for b in remoção:
+   if b > 10 and b < 20:
+        intervalo.append(b)
+        remoção.remove(b)
+        pass
+if len(intervalo) == 0:
+        print("A lista não apresenta elementos no intervalo entre 10 e 20.")
+        pass
+print(remoção)
+# C) Informando quantas vezes um numero aparece na lista.
+repetição = 0
+lista = set(copia_lista)
+for c in lista:
+        repetição = copia_lista.count(c)
+        print("O número %s aparece %s vez(es)." %(c,repetição))
+        pass
