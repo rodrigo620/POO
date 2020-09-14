0efe31eceb5612a2f2688e3810721472e818fadd
rodrigo620 <rs4812575@gmail.com>
2020-09-13 21:13:35 -0300

    Questão 2 completa

--- Trabalho-1/quest_02.py
+++ Trabalho-1/quest_02.py
@@ -1,33 +1,38 @@
 #Capturando valores da lista e fazendo uma copia da mesma.
 numeros = []
 for voltar in range(10):
-    valor = int(input("numero "))
-    numeros.append(valor)
+        valor = int(input("numero "))
+        numeros.append(valor)
 copia_lista = numeros [:]
+
+
 # A) Exibindo os indices do numeros maiores que 50.
 maior50 = []
-for a in copia_lista:
-    if a > 50:
-        maior50.append(a)
-        pass
+for voltas in range(len(copia_lista)):
+        indici = copia_lista[voltas]
+        if indici > 50:
+                maior50.append(voltas)
 if len(maior50) != 0:
         print(maior50)
-        pass
-if len(maior50) == 0:
-    print("Alista não apresenta elementos maiores que o número 50.")
-    pass
+else:
+        print("Alista não apresenta elementos maiores que o número 50.")
+
+
+
 # B) Removendo elementos entre 10 e 20.
 intervalo = []  
 remoção = copia_lista [:]
-for b in remoção:
-   if b > 10 and b < 20:
-        intervalo.append(b)
-        remoção.remove(b)
-        pass
+for b in range(len(copia_lista)):
+        remover = copia_lista[b]
+        if remover > 10 and remover < 20:
+                remoção.remove(remover)
+                intervalo.append(remover)
 if len(intervalo) == 0:
         print("A lista não apresenta elementos no intervalo entre 10 e 20.")
         pass
 print(remoção)
+
+
 # C) Informando quantas vezes um numero aparece na lista.
 repetição = 0
 lista = set(copia_lista)
